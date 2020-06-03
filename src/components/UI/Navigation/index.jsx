968a4bb7e58c3ae2cd668b75a457b667ad7e521e
Andrey <a.kovalenok679@gmail.com>
2020-06-03 02:06:52 +0200

    Адаптив

--- src/components/UI/Navigation/index.jsx
+++ src/components/UI/Navigation/index.jsx
@@ -2,26 +2,30 @@ import React from "react";
 
 import classes from "./Navigation.module.scss";
 
-const Navigation = () => (
-  <nav className={classes.Navigation}>
-    <ul className={classes.Navigation__list}>
-      <li className={classes.Navigation__item}>
-        <a className={classes.Navigation__link} href="/">
-          Купить
-        </a>
-      </li>
-      <li className={classes.Navigation__item}>
-        <a className={classes.Navigation__link} href="/">
-          Снять
-        </a>
-      </li>
-      <li className={classes.Navigation__item}>
-        <a className={classes.Navigation__link} href="/">
-          Наши агенты
-        </a>
-      </li>
-    </ul>
-  </nav>
-);
+const Navigation = () => {
+  return (
+    <>
+      <nav className={classes.Navigation}>
+        <ul className={classes.Navigation__list}>
+          <li className={classes.Navigation__item}>
+            <a className={classes.Navigation__link} href="/">
+              Купить
+            </a>
+          </li>
+          <li className={classes.Navigation__item}>
+            <a className={classes.Navigation__link} href="/">
+              Снять
+            </a>
+          </li>
+          <li className={classes.Navigation__item}>
+            <a className={classes.Navigation__link} href="/">
+              Наши агенты
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
 
 export default Navigation;
