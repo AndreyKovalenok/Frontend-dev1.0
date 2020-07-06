071bbc095f014fa765a14c526039c133ccfd6b54
Andrey <a.kovalenok679@gmail.com>
2020-07-06 14:48:45 +0200

    Updates

---
+++ precache-manifest.208b47cb830f44b51bb7ec0767af44c4.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6582f7aff2800fb77894d85951d0d20",
+    "url": "/Frontend-dev1.0/index.html"
+  },
+  {
+    "revision": "997d8d8b921eb6b62f0f",
+    "url": "/Frontend-dev1.0/static/css/main.f410e681.chunk.css"
+  },
+  {
+    "revision": "e61c8c2d5384fc6d609f",
+    "url": "/Frontend-dev1.0/static/js/2.69df863c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Frontend-dev1.0/static/js/2.69df863c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "997d8d8b921eb6b62f0f",
+    "url": "/Frontend-dev1.0/static/js/main.53f18d81.chunk.js"
+  },
+  {
+    "revision": "27370b4ae71156bbb4d9",
+    "url": "/Frontend-dev1.0/static/js/runtime-main.536839e8.js"
+  },
+  {
+    "revision": "692ddb5d361a990f248afd44fb687314",
+    "url": "/Frontend-dev1.0/static/media/FiraSans-Bold.692ddb5d.woff"
+  },
+  {
+    "revision": "e856f591a0f778f1d1cd058fdafb2be6",
+    "url": "/Frontend-dev1.0/static/media/FiraSans-Bold.e856f591.woff2"
+  },
+  {
+    "revision": "bd6b42666032c6e65b313a1a2a91f736",
+    "url": "/Frontend-dev1.0/static/media/FiraSans-Light.bd6b4266.woff2"
+  },
+  {
+    "revision": "c427d9012ecfe2812cf4d115d6769fe0",
+    "url": "/Frontend-dev1.0/static/media/FiraSans-Light.c427d901.woff"
+  },
+  {
+    "revision": "0db734f69b6c48c562301f1c9a8598ad",
+    "url": "/Frontend-dev1.0/static/media/FiraSans-Medium.0db734f6.woff2"
+  },
+  {
+    "revision": "fea5ce1fa0b6af59b5f222b2f6edd659",
+    "url": "/Frontend-dev1.0/static/media/FiraSans-Medium.fea5ce1f.woff"
+  },
+  {
+    "revision": "4dae0def50cdfde5ec29d90902f51774",
+    "url": "/Frontend-dev1.0/static/media/FiraSans-Regular.4dae0def.woff"
+  },
+  {
+    "revision": "626effefb29c24a6474b7bbeb601f729",
+    "url": "/Frontend-dev1.0/static/media/FiraSans-Regular.626effef.woff2"
+  },
+  {
+    "revision": "9c6b469c8618321232db3371881e7813",
+    "url": "/Frontend-dev1.0/static/media/Monaco.9c6b469c.woff2"
+  },
+  {
+    "revision": "f45134cb8418c5821a164941411364c0",
+    "url": "/Frontend-dev1.0/static/media/Monaco.f45134cb.woff"
+  },
+  {
+    "revision": "361b60253f2f4ec9fc01dd4fb682ee36",
+    "url": "/Frontend-dev1.0/static/media/Philosopher-Bold.361b6025.woff"
+  },
+  {
+    "revision": "ce521496a9127c315ff04c801621d103",
+    "url": "/Frontend-dev1.0/static/media/Philosopher-Bold.ce521496.woff2"
+  },
+  {
+    "revision": "01577d8d989998d711c78c8e2b5c1479",
+    "url": "/Frontend-dev1.0/static/media/bitmap@3x.01577d8d.jpg"
+  },
+  {
+    "revision": "653c2cd2cdab75265168cb5a508cbfff",
+    "url": "/Frontend-dev1.0/static/media/combined-shape.653c2cd2.svg"
+  },
+  {
+    "revision": "85ee97f7cd259841448c89069b104b0a",
+    "url": "/Frontend-dev1.0/static/media/compass-development-logo.85ee97f7.svg"
+  },
+  {
+    "revision": "8073f05c4eef0c46109816071ee769cd",
+    "url": "/Frontend-dev1.0/static/media/compass-logo.8073f05c.svg"
+  },
+  {
+    "revision": "9fe7f05630a493cdecae05c69b7ae093",
+    "url": "/Frontend-dev1.0/static/media/img1.9fe7f056.jpg"
+  },
+  {
+    "revision": "d0c9d615e07c8bee5960a0bbb0c0d7ce",
+    "url": "/Frontend-dev1.0/static/media/img2.d0c9d615.jpg"
+  },
+  {
+    "revision": "8bcdcd08b9841cd5f0a2620785b7a655",
+    "url": "/Frontend-dev1.0/static/media/img3.8bcdcd08.jpg"
+  },
+  {
+    "revision": "0612f3a1735708b3df41fa7dc2c56d70",
+    "url": "/Frontend-dev1.0/static/media/img4.0612f3a1.jpg"
+  },
+  {
+    "revision": "03b6f2cd71afc3a5743385510e1d80d4",
+    "url": "/Frontend-dev1.0/static/media/img5.03b6f2cd.jpg"
+  },
+  {
+    "revision": "af6ad9b2625dad63cf95eda00147d912",
+    "url": "/Frontend-dev1.0/static/media/map@3x.af6ad9b2.png"
+  },
+  {
+    "revision": "e2d8de49c9c13d6504d39ef8c5f307a2",
+    "url": "/Frontend-dev1.0/static/media/polyanka-photo@3x.e2d8de49.jpg"
+  }
+]);
